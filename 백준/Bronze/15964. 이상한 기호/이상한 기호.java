32a21e8328cb33ca12e5873c18177861b1a40ed9
bxkxxng <57355110+bxkxxng@users.noreply.github.com>
2024-03-07 11:21:29 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14244 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
 
         long result = (A + B) * (A - B);
         System.out.println(result);
