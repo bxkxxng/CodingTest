4de1906e9ce343a4105f7237d2e73a4b2f48be59
bxkxxng <57355110+bxkxxng@users.noreply.github.com>
2024-03-12 10:27:47 +0900

    [Silver V] Title: 단어 정렬, Time: 720 ms, Memory: 25716 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        String[] word = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            word[i] = br.readLine();
+        }
+        Arrays.stream(word)
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length).thenComparing(String::compareTo))
+                .forEach(System.out::println);
+    }
+}
