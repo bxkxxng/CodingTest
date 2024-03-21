c0bab8c71b265a75fb4be1b0bb639a3a85f12317
bxkxxng <57355110+bxkxxng@users.noreply.github.com>
2024-03-21 16:07:47 +0900

    Update 알파벳 찾기.java

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -7,16 +7,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
-        int[] alphabetList = new int[26];
+        int[] alphabetArray = new int[26];
 
-        Arrays.fill(alphabetList, -1);
+        Arrays.fill(alphabetArray, -1);
 
         for (int i = 0; i < S.length(); i++) {
             char alphabet = S.charAt(i);
-            if(alphabetList[alphabet - 97] == -1) {
-                alphabetList[alphabet - 97] = i;
+            if(alphabetArray[alphabet - 97] == -1) {
+                alphabetArray[alphabet - 97] = i;
             }
         }
-        Arrays.stream(alphabetList).forEach(i -> System.out.print(i+" "));
+        Arrays.stream(alphabetArray).forEach(i -> System.out.print(i+" "));
     }
 }
