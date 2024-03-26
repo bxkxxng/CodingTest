5ec1b869c63e2b204a689be8d3d71f872f18beff
bxkxxng <57355110+bxkxxng@users.noreply.github.com>
2024-03-26 09:44:23 +0900

    [Silver II] Title: 랜선 자르기, Time: 184 ms, Memory: 17516 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -4,48 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static long binary_search(long arr[], int N, long max) {
-		long half = 0;
-		long min = 1;
+    private static long searchSize(long arr[], int N, long max) {
+        long mid = 0;
+        long min = 1;
 
-		while(min <= max) {
-			half = (min + max)/2;
-			long count = 0;
+        while (min <= max) {
+            mid = (min + max) / 2;
+            long count = 0;
 
-			for(long num : arr) {
-				count += num / half;
-			}
+            for (long num : arr) {
+                count += num / mid;
+            }
 
-			if(count < N) {
-				max = half-1;
-			}
+            if (count < N) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return (max + min) / 2;
+    }
 
-			else {
-				min = half+1;
-			}
-		}
-		return (max+min)/2;
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        long result = 0;
+        long max = 0;
 
-		int K = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		long result = 0;
-		long max = 0;
+        long[] arr = new long[K];
+        for (int i = 0; i < K; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
 
-		long [] arr= new long[K];
-		for(int i=0; i<K; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			max = Math.max(max, arr[i]);
-		}
+        result = searchSize(arr, N, max);
 
-		result = binary_search(arr, N, max);
-
-		System.out.println(result);
-		br.close();
-	}
+        System.out.println(result);
+    }
 }
