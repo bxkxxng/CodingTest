00504be3bad095d47ac0ac60eb2b21a53a426514
bxkxxng <57355110+bxkxxng@users.noreply.github.com>
2024-04-15 10:33:42 +0900

    [Silver IV] Title: 큐, Time: 376 ms, Memory: 18948 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<String> queue = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push" -> queue.offer(st.nextToken());
+                case "pop" -> {
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.peek());
+                        queue.remove();
+                    }
+                }
+                case "size" -> System.out.println(queue.size());
+                case "empty" -> System.out.println(queue.isEmpty() ? 1 : 0);
+                case "front" -> {
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(queue.peek());
+                    }
+                }
+                case "back" -> {
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(((LinkedList<String>) queue).peekLast());
+                    }
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + br.readLine());
+            }
+        }
+    }
+}
