150f2842f9c51826849dd5688ed68560689574da
bxkxxng <57355110+bxkxxng@users.noreply.github.com>
2024-05-22 16:54:30 +0900

    [Silver III] Title: 통계학, Time: 900 ms, Memory: 73016 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> oddMap = new HashMap<>();
+        int[] odd = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            odd[i] = Integer.parseInt(br.readLine());
+            if (oddMap.containsKey(odd[i])) {
+                oddMap.put(odd[i], oddMap.get(odd[i]) + 1);
+            } else {
+                oddMap.put(odd[i], 1);
+            }
+        }
+
+        int avg = getAvg(odd);
+        int mid = getMid(odd);
+        int count = getMaxCount(oddMap);
+        int range = getRange(odd);
+
+        System.out.println(avg);
+        System.out.println(mid);
+        System.out.println(count);
+        System.out.println(range);
+    }
+
+    static int getAvg(int[] arr) {
+        return (int) Math.round(Arrays.stream(arr).average().getAsDouble());
+    }
+
+    static int getMid(int[] arr) {
+        arr = Arrays.stream(arr).sorted().toArray();
+        return arr[arr.length / 2];
+    }
+
+    static int getMaxCount(Map<Integer, Integer> map) {
+        int highCount = Collections.max(map.values());
+        List<Integer> matCount = new ArrayList<>();
+
+        for (Map.Entry<Integer, Integer> newMap : map.entrySet()) {
+            if (newMap.getValue() == highCount) {
+                matCount.add(newMap.getKey());
+            }
+        }
+        Collections.sort(matCount);
+        if (matCount.size() > 1) {
+            return matCount.get(1);
+        } else {
+            return matCount.get(0);
+        }
+    }
+
+    static int getRange(int[] arr) {
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+        return max - min;
+    }
+}
