5b5612aa2144619a2be7949af2a35ed0eaaa941a
bxkxxng <57355110+bxkxxng@users.noreply.github.com>
2024-07-03 16:12:20 +0900

    [Bronze IV] Title: Rust Study, Time: 136 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/30033. Rust Study/Rust Study.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] B = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (A[i] <= B[i]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
